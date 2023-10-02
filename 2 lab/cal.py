e896b2a21f7c1ca6bd9f4ebfefa4f3d5a00471cd
0pavel1 <122034215+0pavel1@users.noreply.github.com>
2023-10-02 11:11:50 +0300

    cal.py

--- 2 lab/cal.py
+++ 2 lab/cal.py
@@ -19,7 +19,10 @@ def otv(data = Body()):
     elif sign == "-":
         return {"st": f"{num1} - {num2} = {num1 - num2}"}
     elif sign == "/":
-        return {"st": f"{num1} / {num2} = {num1 / num2}"}
+        if num2 != 0:
+            return {"st": f"{num1} / {num2} = {num1 / num2}"}
+        else:
+            return {"st": f"На ноль делить нельзя!"}
     elif sign == "*":
         return {"st": f"{num1} * {num2} = {num1 * num2}"}
     else:
