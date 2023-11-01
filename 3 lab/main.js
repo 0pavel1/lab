9fe4cbe29933deacdf886d1ef0fc4ab39740d2e7
0pavel1 <pavel.zamorskiy@mail.ru>
2023-11-02 01:46:21 +0300

    add scripts for buttons

--- 3 lab/main.js
+++ 3 lab/main.js
@@ -4,14 +4,28 @@ function hide(object,trigger) {
     Btn.addEventListener("click", function() {Block.classList.toggle("hide");})
     }
 hide(".menu__list", ".menu__btn")
-document.querySelector(".portfolio__btn").addEventListener("click", function() {document.getElementById("B").style.background = "#FFF";})
-hide(".container", ".portfolio__btn")
-hide(".containerPort", ".portfolio__btn")
-document.querySelector(".portfolio__btn").addEventListener("click", function() {document.querySelector(".portfolio__btn").classList.toggle("hide");})
-document.querySelector(".portfolio__btn").addEventListener("click", function() {document.querySelector(".info__btn").classList.toggle("hide");})
-document.querySelector(".info__btn").addEventListener("click", function() {document.getElementById("B").style.background = "#000";})
-hide(".container", ".info__btn")
-hide(".containerPort", ".info__btn")
-document.querySelector(".info__btn").addEventListener("click", function() {document.querySelector(".portfolio__btn").classList.toggle("hide");})
-document.querySelector(".info__btn").addEventListener("click", function() {document.querySelector(".info__btn").classList.toggle("hide");})
+const portBtns = document.getElementsByClassName("portfolio__btn")
+Array.from(portBtns).forEach(portBtn => {
+  portBtn.addEventListener("click", function() {
+    document.querySelector("#B").style.background = "#FFF";
+    document.getElementById("portfolio").classList.toggle("hide");
+    document.getElementById("info").classList.toggle("hide");
+    document.querySelector(".container").classList.toggle("hide");
+    document.querySelector(".containerPort").classList.toggle("hide");
+    if (!(Array.from(document.querySelector(".menu__list").classList).includes('hide'))){
+        document.querySelector(".menu__list").classList.toggle("hide");
+        };
+    });
+});
+const infoBtns = document.getElementsByClassName("info__btn")
+Array.from(infoBtns).forEach(infoBtn => {
+  infoBtn.addEventListener("click", function() {
+    document.querySelector("#B").style.background = "#000";
+    document.getElementById("portfolio").classList.toggle("hide");
+    document.getElementById("info").classList.toggle("hide");
+    document.querySelector(".container").classList.toggle("hide");
+    document.querySelector(".containerPort").classList.toggle("hide");
+    });
+});
+
 
